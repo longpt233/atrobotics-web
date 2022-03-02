5513a3a86937ec3da57aaa6cd9ef95b0e3b91bbc
longpt233 <driverofl01@gmail.com>
2022-03-02 10:12:21 +0700

    check port

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express(); 
+const PORT = 80;
 
 app.get('/', function(req, res){
     res.send("Hello World");
 })
 
-app.listen()
+app.listen(PORT, () => {
+    console.log(`Server running in port ${PORT}`)
+})
