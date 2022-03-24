2e63c168f7a260cc3df49f2770f26f2d62a2b4de
Phan Thanh Long <longphanthanh@tech.admicro.vn>
2022-03-24 18:10:30 +0700

    url

--- server/internal/handler/fileUpload.go
+++ server/internal/handler/fileUpload.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -29,12 +30,15 @@ func SingleFile(c *gin.Context) {
 	savePath := os.Getenv("IMAGE_SAVE_PATH")
 	log.Println(file.Filename + "save at: " + savePath)
 
-	err = c.SaveUploadedFile(file, savePath+ nameFile)
+	extSplit := strings.Split(file.Filename, ".")
+	ext := extSplit[len(extSplit)-1]
+
+	err = c.SaveUploadedFile(file, savePath+nameFile+"."+ext)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, helper.BuildResponse(-1, "lấy dc ảnh nhưng lỗi cmnr khi savefile", err.Error()))
 		return
 	}
 
-	c.JSON(http.StatusOK, helper.BuildResponse(1, "get file done", nameFile))
+	c.JSON(http.StatusOK, helper.BuildResponse(1, "get file done", "http://atroboticsvn.com/static/images/"+nameFile+"."+ext))
 
 }
