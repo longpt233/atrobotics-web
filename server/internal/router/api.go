db4c5d16133060b736a6b7e570a8cad604c60295
longpt <longphanthanh@tech.admicro.vn>
2022-03-30 23:05:37 +0700

    add cors

--- server/internal/router/api.go
+++ server/internal/router/api.go
@@ -6,12 +6,15 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	cors "github.com/rs/cors/wrapper/gin"
 )
 
 //RunAPI ->route setup
 func RunAPI(address string) error {
 
 	r := gin.Default()
+
+	r.Use(cors.Default())
 
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.String(http.StatusOK, "Welcome to Our Mini Ecommerce")
