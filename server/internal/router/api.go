4733fe4007795528819192f39d0367a69482e198
longpt <longphanthanh@tech.admicro.vn>
2022-03-31 09:45:16 +0700

    add cors

--- server/internal/router/api.go
+++ server/internal/router/api.go
@@ -94,7 +94,7 @@ func corsMiddleware() gin.HandlerFunc {
 
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
