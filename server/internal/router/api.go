b18243f8907e67fae3ea72ac7e2554150a3c7605
longpt <longphanthanh@tech.admicro.vn>
2022-03-31 10:16:52 +0700

    add cors

--- server/internal/router/api.go
+++ server/internal/router/api.go
@@ -4,6 +4,7 @@ import (
 	"atro/internal/handler"
 	"atro/internal/middleware"
 	"net/http"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -14,7 +15,17 @@ func RunAPI(address string) error {
 
 	r := gin.Default()
 
-	r.Use(cors.Default())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "*"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	// r.Use(corsMiddleware())
 
 	r.GET("/", func(ctx *gin.Context) {
