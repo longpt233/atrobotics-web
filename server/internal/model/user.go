07232c7b872facf6c26ca3e54ec3857cfa4099c6
Tuan Nguyen <tuantoquq@gmail.com>
2022-04-23 23:19:56 +0700

    modify user attribute gorm column

--- server/internal/model/user.go
+++ server/internal/model/user.go
@@ -6,8 +6,8 @@ type User struct {
 	UserRoleID    string `json:"roleId" gorm:"column:user_role_id"`                                      // col name
 	UserEmail     string `json:"email" binding:"required,email" gorm:"unique" gorm:"column:user_email"` //TODO:  binding ?
 	UserPassword  string `json:"password" binding:"required" gorm:"column:user_password"`
-	UserFirstName string `json:"firstName" gorm:"column:user_firstname"`
-	UserLastName  string `json:"lastName" gorm:"column:user_lastname"`
+	UserFirstName string `json:"firstName" gorm:"column:user_first_name"`
+	UserLastName  string `json:"lastName" gorm:"column:user_last_name"`
 	UserPhone     string `json:"phone" gorm:"column:user_phone"`
 	UserAddress   string `json:"address" gorm:"column:user_address"`
 }
