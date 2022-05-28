b863b7705534ec4955ca7b06e170fdff72900810
Tuan Nguyen <tuantoquq@gmail.com>
2022-05-28 15:32:29 +0700

    fix add cart API

--- server/internal/model/cartItems.go
+++ server/internal/model/cartItems.go
@@ -5,14 +5,14 @@ import (
 )
 
 type CartItems struct {
-	CartId          string    `json:"id" gorm:"column:cart_id" gorm:"primaryKey"`
-	CartUserId      string    `json:"userId" gorm:"column:cart_user_id"`
-	CartProductId   string    `json:"productId" gorm:"column:cart_product_id"`
-	CartProductData interface{}   `json:"productData"`
-	CartQuantity    int       `json:"quantity" gorm:"column:cart_quantity"`
-	CartCreatedAt   time.Time `json:"createdAt" gorm:"column:cart_created_at"`
-	CartUpdatedAt   time.Time `json:"updateAt" gorm:"column:cart_updated_at"`
-	CartColor       string    `json:"color" gorm:"column:cart_color"`
+	CartId          string      `json:"id" gorm:"column:cart_id" gorm:"primaryKey"`
+	CartUserId      string      `json:"userId" gorm:"column:cart_user_id"`
+	CartProductId   string      `json:"productId" gorm:"column:cart_product_id"`
+	CartProductData interface{} `json:"productData" gorm:"column:cart_product_data"`
+	CartQuantity    int         `json:"quantity" gorm:"column:cart_quantity"`
+	CartCreatedAt   time.Time   `json:"createdAt" gorm:"column:cart_created_at"`
+	CartUpdatedAt   time.Time   `json:"updateAt" gorm:"column:cart_updated_at"`
+	CartColor       string      `json:"color" gorm:"column:cart_color"`
 }
 
 func (CartItems) TableName() string {
