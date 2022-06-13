86671fb8b4ae5c390a4bfbaecd7cea8381f8e740
longpt <longphanthanh@tech.admicro.vn>
2022-06-13 20:17:26 +0700

    fix db

--- server/internal/repository/db.go
+++ server/internal/repository/db.go
@@ -4,37 +4,51 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
-
 type MySQLClient struct {
-	db *gorm.DB
 }
 
+var lock = &sync.Mutex{}
+var db *gorm.DB
 
 //TODO: singleton, close connection
 func (myclient *MySQLClient) GetConn() *gorm.DB {
 
-	conn := os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("ATRO_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?parseTime=true"
+	// db.LogMode(true) // show query log
 
+	// db.AutoMigrate()
+	if db == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		if db == nil {
 
-	db, err := gorm.Open("mysql", conn)
-	if err != nil {
-		log.Fatal("Error connecting to Database" + err.Error())
-		return nil
+			fmt.Println("Creating single instance now.")
+			conn := os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("ATRO_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME") + "?parseTime=true"
+
+			var err error
+			db, err = gorm.Open("mysql", conn)
+			if err != nil {
+				log.Fatal("Error connecting to Database" + err.Error())
+				return nil
+			} else {
+				fmt.Println("Open conn successfully to " + conn)
+			}
+
+		} else {
+			fmt.Println("Single instance already created.")
+		}
 	} else {
-		fmt.Println("Open conn successfully to " + conn)
+		fmt.Println("Single instance already created.")
 	}
 
-	db.LogMode(true) // show query log
-	db.AutoMigrate()
 	return db
 }
 
-func (myclient *MySQLClient) Close(){
-	myclient.db.Close()
-}
-
+// func (myclient *MySQLClient) Close() {
+// 	myclient.db.Close()
+// }
